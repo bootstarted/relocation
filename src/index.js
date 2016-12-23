05855b1ea751688775b6edbd33a8f4ac4267ba82
Neal Granger <neal@metalab.co>
2016-12-22 22:53:54 -0800

    Remove React Router integration.
    Remove `RelocationRouter` component and related logic from state and connect component. Tightly binding this lib to React Router is not the best idea.
    
    The behavior removed here can be easily replicated by manually dispatching `setComponent` from the `componentWillMount` and `removeComponent` from the `componentWillUnmount` method of a route handler component.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ export {ADD_COMPONENT, REMOVE_COMPONENT} from './action';
 export {addComponent, removeComponent} from './action';
 export {default as relocation} from './connect';
 export {default as reducer} from './reducer';
-export {default as createRelocationRouter} from './router';
 export {default} from './connect';
