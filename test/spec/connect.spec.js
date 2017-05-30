92a20b8af1f36b7cce87ff4364971aeb95c747c8
Izaak Schroeder <izaak.schroeder@gmail.com>
2017-05-29 17:55:01 -0700

    Very primitive connect test.

--- test/spec/connect.spec.js
+++ test/spec/connect.spec.js
@@ -1,6 +1,33 @@
-import {shallow} from 'enzyme';
-import expect from 'unexpected';
+import _expect from 'unexpected';
+import react from 'unexpected-react/test-renderer';
+import TestRenderer from 'react-test-renderer';
+import {createElement} from 'react';
+import {createStore as baseCreateStore, combineReducers} from 'redux';
+import relocation from '../../src/reducer';
+import connect from '../../src/connect';
+import {setComponent} from '../../src/action';
+
+const expect = _expect.clone().use(react);
+
+const Base = ({components:[{id, props: {foo}}]}) => <div>{id} - {foo}</div>;
+
+const createStore = () => baseCreateStore(combineReducers({relocation}));
 
 describe('connect', () => {
-
+  it('should do the needful', () => {
+    const store = createStore();
+    const Component = connect()(Base);
+    store.dispatch(setComponent('TEST', 'banana', {foo: 5}));
+    const result = TestRenderer.create(
+      <Component
+        components={{
+          TEST: ({foo}) => <div>Hi</div>
+        }}
+        store={store}
+      />
+    );
+    expect(result, 'to have rendered', (
+      <div>banana - 5</div>
+    ));
+  });
 });
