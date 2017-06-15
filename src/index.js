dcc67ef2d4c31a4c87386b4c0e2126228aeec437
Izaak Schroeder <izaak.schroeder@gmail.com>
2017-06-14 20:22:42 -0700

    Add missing action exports to index.
    Handy to have these as imports.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-export {ADD_COMPONENT, REMOVE_COMPONENT} from './action';
-export {addComponent, removeComponent} from './action';
+export {
+  ADD_COMPONENT,
+  SET_COMPONENT,
+  UPDATE_COMPONENT,
+  REMOVE_COMPONENT,
+} from './action';
+export {
+  addComponent,
+  removeComponent,
+  setComponent,
+  updateComponent,
+} from './action';
 export {default as relocation} from './connect';
 export {default as reducer} from './reducer';
 export {default} from './connect';
